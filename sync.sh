954ecb12fd43b2c3463813e460b25e36d8f7228d
sas-sajjad <143927318+sas-sajjad@users.noreply.github.com>
2023-09-04 12:33:42 +0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ rm -rf .repo/local_manifests
 command=$(head $CIRRUS_WORKING_DIR/config.sh -n $(expr $(grep 'build/envsetup.sh' $CIRRUS_WORKING_DIR/config.sh -n | cut -f1 -d:) - 1))
 only_sync=$(grep 'repo sync' $CIRRUS_WORKING_DIR/config.sh)
 bash -c "$command" || true
+
